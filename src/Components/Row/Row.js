7ec9aa32bc577ddc906210487c9692dd6b83351b
Aaron Pruzaniec <aaronpruzaniec@Aarons-MacBook-Pro.local>
2022-01-07 01:39:04 -0500

    Local state accessible

--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 function Row(props) {
-  const [rowData, setRowData] = useState({});
+  const [rowData, setRowData] = useState(props);
+  const [test, setTest] = useState(false);
 
   useEffect(() => {
     setRowData(props);
@@ -12,14 +13,16 @@ function Row(props) {
     let a = props.state;
     a[props.name] = true;
     props.setState(a);
-    setRowData(a);
+    setTest(true);
   }
   return (
     <div
       className={`row ${props.state[props.name] ? "selected" : null}`}
       onClick={handleChange}
     >
-      <div className={`c1`}>[{rowData.props ? "0" : "1"}]</div>
+      <div className={`c1`}>
+        [{test} {props.state ? "0" : "1"}]
+      </div>
       <div className={`c2`}>{rowData.name}</div>
       <div className={`c3`}>{rowData.device}</div>
       <div className={`c4`}>{rowData.path}</div>
