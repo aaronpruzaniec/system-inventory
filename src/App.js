ca399830a55ad2eee70c1e5eb35fc88731b7a4b8
Aaron Pruzaniec <aaronpruzaniec@Aarons-MacBook-Pro.local>
2022-01-06 19:06:18 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const data = [
+    {
+      name: "smss.exe",
+      device: "Stark",
+      path: "\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe",
+      status: "scheduled",
+    },
+    {
+      name: "netsh.exe",
+      device: "Targaryen",
+      path: "\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe",
+      status: "available",
+    },
+    {
+      name: "uxtheme.dll",
+      device: "Lanniester",
+      path: "\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll",
+      status: "available",
+    },
+    {
+      name: "cryptbase.dll",
+      device: "Martell",
+      path: "\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll",
+      status: "scheduled",
+    },
+    {
+      name: "7za.exe",
+      device: "Baratheon",
+      path: "\\Device\\HarddiskVolume1\\temp\\7za.exe",
+      status: "scheduled",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="flex c1">Name</div>
+        <div className="flex c2">Device</div>
+        <div className="flex c3">Path</div>
+        <div className="flex c4">Status</div>
       </header>
     </div>
   );
